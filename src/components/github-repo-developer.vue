8c3889367ecbabfbbf505dbf63f9251cfe6f5c08
Alexei <alexeimyshkouski@gmail.com>
2023-11-20 21:18:56 +0000

    fix color

--- src/components/github-repo-developer.vue
+++ src/components/github-repo-developer.vue
@@ -3,7 +3,7 @@
 .flex.items-center.space-x-4
   NuxtLink(:to="profile?.html_url" rel="external" target="_blank" class="hover:underline")
     .avatar
-      .w-12.rounded-full.ring.ring-neutral-content.ring-offset-base-100.ring-offset-2(class="hover:ring-offset-4 transition-all")
+      .w-12.rounded-full.ring.ring-neutral.ring-offset-base-100.ring-offset-2(class="hover:ring-offset-4 transition-all")
         img(:src="avatarUrl")
   NuxtLink(:to="profile?.html_url" rel="external" target="_blank" class="hover:underline")
     p.font-bold
